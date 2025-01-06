d3acaa85763379aad1cdc657c5c974ae14c98446
Abubokkor98 <mail.abubokkor@gmail.com>
2025-01-07 00:36:00 +0600

    68-6 Make User Admin and install JWT

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -79,7 +80,7 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
-    app.delete("/user/:id", async (req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = {
         _id: new ObjectId(id),
