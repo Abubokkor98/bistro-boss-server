bcfed1568f178c285b1c762fef909b95b862d1af
Abubokkor98 <mail.abubokkor@gmail.com>
2025-01-14 12:04:17 +0600

    create payment api for decoded user

--- index.js
+++ index.js
@@ -220,6 +220,14 @@ async function run() {
       const deleteResult = await cartCollection.deleteMany(query);
       res.send({ paymentResult, deleteResult });
     });
+    app.get("/payment/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
